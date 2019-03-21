a6d0fc09c93d87a4a1617b6122d9db0260a15258
TFlearyDev <tflearydev@gmail.com>
2019-03-21 11:53:35 -0400

    coded connection.js file to set up mongoose connection to mongodb

---
+++ db/connection.js
@@ -0,0 +1,22 @@
+// connecting mongoose and mongodb
+// setting up mongoose connection
+
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+if(process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect('mongodb://localhost/idea-board')
+}
+
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error: ', err)
+    process.exit(-1)
+})
+
+mongoose.connection.once('open', () => {
+    console.log("Mongoose has connected to MongoDB")
+})
+
+module.exports = mongoose
