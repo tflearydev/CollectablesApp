8a941312c60964edf6a7f3cc6177a3d4a31bd90f
TFlearyDev <tflearydev@gmail.com>
2019-03-22 13:00:29 -0400

    added user controller into my server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-// const userController = require('./controllers/userController.js')
+const userController = require('./controllers/userController.js')
 
 app.use(express.json())
 app.use(express.static(__dirname + '/client/build/'));
 
-// app.use('/api/users', userController)
+app.use('/api/users', userController)
 
 app.get('/', (req,res) => {
     res.sendFile(__dirname + '/client/build/index.html')
