9b93b2a7227aa25ed3df5ab8a3b38c7ff1752ede
TFlearyDev <tflearydev@gmail.com>
2019-03-25 10:44:42 -0400

    editied index.js to add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
+import NavBar from './components/NavBar.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // import axios from "axios";
 // here I was going to import some data but not sure yet
 
-import NavBar from './components/NavBar.js'
+
 import HomePage from './components/HomePage.js'
 import UserPage from './components/UserPage.js'
 import SneakerPage from './components/SneakerPage.js'
