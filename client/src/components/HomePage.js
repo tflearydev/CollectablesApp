5eee630f53ba2e41f6fdf14dfeccf98f9026cab5
TFlearyDev <tflearydev@gmail.com>
2019-03-25 10:32:10 -0400

    updated homepage.js

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Sneaker Colle¢t</h1>
+                <Link to="/user">Enter Here</Link>
+            </div>
+        )
+    }
+}
+
+export default HomePage
