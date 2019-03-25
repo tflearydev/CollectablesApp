0cb954b5497a6ecc8ca1e2f265afab30c9f054a8
TFlearyDev <tflearydev@gmail.com>
2019-03-25 10:38:50 -0400

    add router to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 // import axios from "axios";
 // here I was going to import some data but not sure yet
 
-
-import NavBar from "./components/NavBar.js";
+import NavBar from './components/NavBar.js'
 import HomePage from './components/HomePage.js'
 import UserPage from './components/UserPage.js'
 import SneakerPage from './components/SneakerPage.js'
@@ -15,18 +14,20 @@ import SneakerPage from './components/SneakerPage.js'
 // remember styled at bottom, check 'diff' way to do it
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-      <NavBar />
-        <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/user" component={UserPage} />
-          <Route path="/user/:userId" component={SneakerPage} />
-        </Switch>
-      </div>
-    );
+      <Router>
+        <div className='App'>
+          <NavBar />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/user' component={UserPage} />
+            <Route path='/user/:userId' component={SneakerPage} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
