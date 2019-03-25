2e013718efbf7cdc63dd6d5ad42e4a74ab7c21d3
TFlearyDev <tflearydev@gmail.com>
2019-03-25 10:25:45 -0400

    updated app.js and imported bootstrap, along with editing routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
-// import Navbar from "./components/Navbar"; plan to make a navbar
-// import HomePage from './components/HomePage.js'
-// import UserPage from './components/UserPage.js'
-// import SneakerPage from './components/SneakerPage.js'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+// import axios from "axios";
+// here I was going to import some data but not sure yet
+
+
+import NavBar from "./components/NavBar.js";
+import HomePage from './components/HomePage.js'
+import UserPage from './components/UserPage.js'
+import SneakerPage from './components/SneakerPage.js'
 
 // import styled from 'styled-components'
 // remember styled at bottom, check 'diff' way to do it
@@ -14,20 +18,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <NavBar />
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/user" component={UserPage} />
+          <Route path="/user/:userId" component={SneakerPage} />
+        </Switch>
       </div>
     );
   }
