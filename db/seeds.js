8eb9676159279fe5cbc4f2296f17cdf2faeaf7fd
TFlearyDev <tflearydev@gmail.com>
2019-03-27 10:06:00 -0400

    dropped database to edit and re seed

--- db/seeds.js
+++ db/seeds.js
@@ -9,7 +9,7 @@ const Sneaker = require('../models/Sneaker')
 
 const yeezy = new Sneaker({
   title: 'Adidas Yeezy Boost 700 (Inertia)',
-  description: 'Sz 10, released 11/5/19, A condition',
+  description: 'Sz 10, released 3/5/19, A condition',
   // brand: 'Adidas',
   // model: 'Yeezy Boost 700',
   // colorway: 'Inertia',
@@ -18,9 +18,9 @@ const yeezy = new Sneaker({
   // condition: 'A',
   // value: 'https://stockx.com/adidas-yeezy-boost-700-inertia'
 })
-const nike = new Sneaker({
-  title: 'Adidas Yeezy Boost 700 (Inertia)',
-  description: 'Sz 10, released 11/5/19, A condition',
+const prestoWhite = new Sneaker({
+  title: 'Nike Air Presto Off-White (White)',
+  description: 'Sz 10.5, released 11/5/18, B+ condition',
   // brand: 'Nike',
   // model: 'Air Presto Off-White',
   // colorway: 'Black',
@@ -29,10 +29,35 @@ const nike = new Sneaker({
   // condition: 'A',
   // value: 'https://stockx.com/nike-air-presto-off-white-black-2018'
 })
+
+const prestoBlack = new Sneaker({
+  title: 'Nike Air Presto Off-White (Black',
+  description: 'Sz 10, released 11/5/18, A condition',
+  // brand: 'Nike',
+  // model: 'Air Presto Off-White',
+  // colorway: 'Black',
+  // size: '10.5',
+  // releaseDate: '11/5/19',
+  // condition: 'A',
+  // value: 'https://stockx.com/nike-air-presto-off-white-black-2018'
+})
+
+const react = new Sneaker({
+  title: 'Nike React Element 87 Sail Light (Bone)',
+  description: 'Sz 9.5, released 9/4/18, A condition',
+  // brand: 'Nike',
+  // model: 'Air Presto Off-White',
+  // colorway: 'Black',
+  // size: '10.5',
+  // releaseDate: '11/5/19',
+  // condition: 'A',
+  // value: 'https://stockx.com/nike-air-presto-off-white-black-2018'
+})
+
 const tray = new User({
   userName: 'tfleary_kicks',
   password: 'greatthings',
-  sneakers: [yeezy, nike]
+  sneakers: [yeezy, prestoWhite, prestoBlack, react]
   // firstName: 'Tray',
   // lastName: 'Fleary',
   // email: 'tflearydev@gmail.com',
