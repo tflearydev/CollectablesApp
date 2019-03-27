41b503a217a8d9c711949272612687cc499639b2
TFlearyDev <tflearydev@gmail.com>
2019-03-27 14:52:17 -0400

    testing jumbotron to home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { Jumbotron } from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
 
 class HomePage extends Component {
   render () {
     return (
-      <div>
+      <Jumbotron>
+          <Image src=""/>
         <h1>Sneaker Colle¢t</h1>
         <Link to='/user'>Enter Here</Link>
-      </div>
+      </Jumbotron>
     )
   }
 };
